409987fc995360956418513ed0ea821542e159cb
Tony Pitale <tpitale@gmail.com>
2018-07-06 07:21:20 -0500

    Encode map params as form body

--- lib/staccato/adapter/http_poison.ex
+++ lib/staccato/adapter/http_poison.ex
@@ -2,7 +2,7 @@ defmodule Staccato.Adapter.HttpPoison do
   # @behaviour Staccato.Adapter
 
   def post(url, params) do
-    case HTTPoison.post url, params do
+    case HTTPoison.post url, URI.encode_www_form(params) do
       {:ok, _response} -> :ok
       {:error, reason} -> {:error, reason}
     end
