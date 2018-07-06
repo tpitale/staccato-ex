c815ccc1f0a817108bbbfc5f913258507d958b50
Tony Pitale <tpitale@gmail.com>
2018-07-05 20:55:11 -0500

    Fix a test I already borked

--- lib/staccato.ex
+++ lib/staccato.ex
@@ -8,8 +8,8 @@ defmodule Staccato do
 
   ## Examples
 
-      iex> Staccato.tracker("X-YYYYY-1")
-      %Staccato.Tracker{id: "X-YYYYY-1"}
+      iex> Staccato.tracker("X-YYYYY-1").id
+      "X-YYYYY-1"
 
       iex> Staccato.tracker(nil)
       %Staccato.NoopTracker{}
@@ -17,6 +17,7 @@ defmodule Staccato do
   """
   def tracker(nil), do: %Staccato.NoopTracker{}
   def tracker(nil, nil), do: tracker(nil)
+  def tracker(id), do: %Staccato.Tracker{id: id, client_id: generate_client_id()}
   def tracker(id, client_id), do: %Staccato.Tracker{id: id, client_id: (client_id || generate_client_id())}
   def tracker(id, client_id, options) when is_map(options) do
     options |> Kernel.struct(tracker(id, client_id))
