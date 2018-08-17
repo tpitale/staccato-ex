e2a148a6c5f4eb8ea090fee05ac78db8c5fc5446
Tony Pitale <tpitale@gmail.com>
2018-08-17 09:33:08 -0500

    Test and support tracker with id and options, default client_id

--- lib/staccato.ex
+++ lib/staccato.ex
@@ -10,11 +10,29 @@ defmodule Staccato do
 
       iex> Staccato.tracker("X-YYYYY-1").id
       "X-YYYYY-1"
+
+      iex> Staccato.tracker(nil, nil, nil)
+      %Staccato.NoopTracker{}
+
+      iex> Staccato.tracker("X-YYYYY-2", %{ssl: true}).ssl
+      true
+
+      iex> Staccato.tracker("X-YYYYY-2", [ssl: true]).ssl
+      true
+
+      iex> Staccato.tracker("X-YYYYY-2", [{:ssl, true}]).ssl
+      true
+
+      iex> Staccato.tracker("X-YYYYY-2", [{:ssl, true}]).client_id != nil
+      true
+
   """
   def tracker(id, client_id \\ UUID.uuid4(), options \\ [])
 
-  def tracker(nil, _, _), do: struct(%Staccato.NoopTracker{})
+  def tracker(nil, _, _), do: %Staccato.NoopTracker{}
 
+  def tracker(id, options, _) when is_map(options), do: tracker(id, UUID.uuid4(), options)
+  def tracker(id, options, _) when is_list(options), do: tracker(id, UUID.uuid4(), options)
   def tracker(id, client_id, []), do: %Staccato.Tracker{id: id, client_id: client_id}
 
   def tracker(id, client_id, options) do
